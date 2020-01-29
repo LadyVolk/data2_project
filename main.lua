5ab60af50d0639bf8df118ecea6c98bb1b5b8fdd
Pedro Arrochela Lobo <40302551+LadyVolk@users.noreply.github.com>
2020-01-29 18:25:48 +0000

    Player can move and fullscreen
    added escape key to close program

--- main.lua
+++ main.lua
@@ -2,9 +2,16 @@ function love.load()
   player = require "player"
 end
 
-function love.update()
+function love.update(dt)
+  player:update(dt)
 end
 
 function love.draw()
   player:draw()
+end
+
+function love.keypressed(key, scancode, isrepeat)
+  if key == "escape" then
+    love.event.quit()
+  end
 end
--- player.lua
+++ player.lua
@@ -1,7 +1,7 @@
 local player = {
   pos = { x = 300, y = 300},
   size = {w = 50, h = 50},
-  speed = 50,
+  speed = 500,
   color = {r = 0.5, g = 1, b = 1},
 
 }
@@ -13,7 +13,23 @@ function player:draw()
                           self.size.w, self.size.h)
 end
 
-function player.update()
+function player:update(dt)
+  player:update_movement(dt)
+end
+
+function player:update_movement(dt)
+  if love.keyboard.isDown("w") then
+    self.pos.y = self.pos.y - self.speed * dt
+  end
+  if love.keyboard.isDown("a") then
+    self.pos.x = self.pos.x - self.speed * dt
+  end
+  if love.keyboard.isDown("s") then
+    self.pos.y = self.pos.y + self.speed * dt
+  end
+  if love.keyboard.isDown("d") then
+    self.pos.x = self.pos.x + self.speed * dt
+  end
 end
 
 return player
